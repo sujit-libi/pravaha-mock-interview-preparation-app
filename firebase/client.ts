90a9459a303372369107419d139db22adbc3d1d8
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-03-31 08:55:06 +0545

    Pravaha Interview

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDuGmcKx2hzuNJGQk1Zv4FCbp59zxEE46I',
+  authDomain: 'pravaha-ai-mock-interview-prep.firebaseapp.com',
+  projectId: 'pravaha-ai-mock-interview-prep',
+  storageBucket: 'pravaha-ai-mock-interview-prep.firebasestorage.app',
+  messagingSenderId: '687529590178',
+  appId: '1:687529590178:web:a2847f4261d016bca6c6fb',
+  measurementId: 'G-M2JR7KP1T3',
 };
 
 // Initialize Firebase
